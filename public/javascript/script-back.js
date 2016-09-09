0febe85992751b5ccbf49d76b78c6dd00507f415
Jon_shen <jerosshen@gmail.com>
2016-09-09 09:18:46 +0800

    各个都是正确的

--- public/javascript/script-back.js
+++ public/javascript/script-back.js
@@ -4,16 +4,47 @@
     var pathname = location.pathname
     var hash = location.hash
 
+    // 登陆页
+    // 登录页面用户名不存在事件
+    if (pathname == "/login")
+        if (hash == "#err") {
+          $('.alert-danger').show();
+          $(".alert-danger .close").click(function() {
+            location.hash = ""
+          })
+        }
+    // 密码事件
+    var inputPass = $('.form-password-1')
+    var relPass = $('.form-password-2')
+
+    // keyup keycode输出都是大写，charcode都是0
+    inputPass.keypress(function(e){
+      var charCode = e.charCode;
+      e.preventDefault();
+      inputPass.val(inputPass.val() + '*');
+      relPass.val(relPass.val()+String.fromCharCode(charCode))
+    })
+
+
+    //登陆页结束
+
     // 当前导航背景
-
     $("#sidebar_left .nav.sidebar-menu>li>a[href='" + pathname + "']" ).parent().addClass('active');
+    // 导航大小
+    var panelWidth = $('#panel-wrap').width();
+    var countNav = $('.sidebar-left-content>.nav>li').length;
+    console.log(countNav);
+    $('.nav>li>a').css('width', panelWidth/countNav);
+    $('body.sb-top #sidebar_left .sidebar-menu > li > ul').css('width', panelWidth/countNav);
 
-    // 提交
+
+    // 提交功能
     $('button[type="submit"]').on('submit', function() {
       $(this).prop("disabled", true)
     })
 
-    // 删除
+
+    // 删除功能
     $('.del').click(function(e) {
       var target = $(e.target)
       var id = target.data('id')
@@ -32,13 +63,16 @@
       })
     })
 
-    // 修改
+
+    // 修改功能
     $('.change').click(function(e) {
       var target = $(e.target)
       var id = target.data('id')
       location.search = "?id=" + id
     })
-    // 查看
+
+
+    // 查看博客
     $('.view_blog').click(function(e) {
       var target = $(e.target)
       var id = target.data('id')
@@ -52,14 +86,7 @@
     //   console.log('f');
     // })
 
-    // 登录页面用户名不存在事件
-    if (pathname == "/login")
-        if (hash == "#err") {
-          $('.alert-danger').show();
-          $(".alert-danger .close").click(function() {
-            location.hash = ""
-          })
-        }
+
 
   // js这里结束
   })
--- public/javascript/script-front.js
+++ public/javascript/script-front.js
@@ -7,9 +7,29 @@
     // 当前导航背景
     $(".menu .navbar-nav > li > a[href='" + pathname + "']" ).parent().addClass('active');
 
+    $('a[data-target="#signinModal"]').click(function(){
+      var $q = $('.heming_content').val();
+      var warn = $('.alert-warning');
+      if (!$q) {
+        if(warn.is(':hidden')){
+          warn.slideDown();
+        }
+        return false
+      }else {
+        if(warn.is(':visible')){
+          $('.alert-warning').slideUp()
+        }
+        $('#clientquery').val($q)
+      }
+    })
+    $('button.close').click(function(){
+      $('.alert-warning').slideUp()
+    })
+
     // 页面载入后幻灯片显示设置
     $('.carousel-inner .item').eq(0).addClass('active')
 
+    // 首页公司核对提交
     $('button.submit').click(function(){
       $('#clientTel').value()
       if(value)
